211b154f05c8a049f44b7053e3d0360159dceeeb
Arnaud Boeglin <a.boeglin67@gmail.com>
2020-10-09 15:26:36 +0200

    chore: Move type types in Type.hs

--- src/Infer.hs
+++ src/Infer.hs
@@ -12,32 +12,7 @@ import           Grammar
 import           Debug.Trace
 import           Data.Foldable                  ( Foldable(foldl') )
 import           Data.Char                      ( isLower )
-
-
-newtype TVar = TV String
-  deriving (Show, Eq, Ord)
-
-data Type
-  = TVar TVar         -- Variable type
-  | TCon TCon         -- Constant type
-  | TArr Type Type    -- Arrow type
-  -- TODO: Maybe move back to TComp TCon [TVar] and construct the Type from TVar
-  -- in needed places
-  -- TODO: Rename TADT ?
-  | TComp TCon [Type] -- Composite type
-  deriving (Show, Eq, Ord)
-
-data TCon
-  = CString
-  | CNum
-  | CBool
-  | CUserDef String
-  deriving (Show, Eq, Ord)
-
-infixr `TArr`
-
-data Scheme = Forall [TVar] Type
-  deriving (Show, Eq, Ord)
+import           Type
 
 -- TODO: Convert back to a simple type
 type Vars = M.Map String Scheme
