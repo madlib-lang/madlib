21cf516b0056f35e6ace56a108f2de04e58acbb4
Arnaud Boeglin <a.boeglin67@gmail.com>
2021-11-27 10:13:05 +0100

    and there were default imports in llvm backend

--- src/Generate/LLVM/Rename.hs
+++ src/Generate/LLVM/Rename.hs
@@ -3,6 +3,7 @@ module Generate.LLVM.Rename where
 
 
 import qualified Data.Map                      as Map
+import qualified Data.Set                      as Set
 import qualified Data.Maybe                    as Maybe
 import qualified Data.Bifunctor                as Bifunctor
 import qualified Data.ByteString.Lazy.Char8    as BLChar8
@@ -18,6 +19,10 @@ data Env
   = Env
     { namesInScope :: Map.Map String String
     , currentModuleHash :: String
+    , usedDefaultImportNames :: Map.Map String (Set.Set String)
+    -- ^ Name of the default import as key set of used names as value, eg. List.filter coming from import List from "List"
+    , defaultImportHashes :: Map.Map String String
+    -- ^ Keys are the imported namespace eg. import List from "List", and the value is the hash of the module
     }
     deriving(Eq, Show)
 
@@ -25,6 +30,17 @@ data Env
 extendScope :: String -> String -> Env -> Env
 extendScope initialName newName env@Env{ namesInScope } =
   env { namesInScope = Map.insert initialName newName namesInScope }
+
+
+addDefaultImportHash :: String -> String -> Env -> Env
+addDefaultImportHash defaultImportName moduleHash env@Env{ defaultImportHashes } =
+  env { defaultImportHashes = Map.insert defaultImportName moduleHash defaultImportHashes }
+
+addDefaultImportNameUsage :: String -> String -> Env -> Env
+addDefaultImportNameUsage defaultImportName objectName env@Env{ usedDefaultImportNames } =
+  let setForName = Maybe.fromMaybe Set.empty $ Map.lookup defaultImportName usedDefaultImportNames
+      extended   = Set.insert objectName setForName
+  in  env { usedDefaultImportNames = Map.insert defaultImportName extended usedDefaultImportNames }
 
 
 addHashToName :: String -> String -> String
@@ -47,11 +63,11 @@ hashModulePath ast =
   generateHashFromPath $ Maybe.fromMaybe "" (apath ast)
 
 
-renameExpsInBody :: Env -> [Exp] -> ([Exp], Env)
-renameExpsInBody env exps = case exps of
+renameExps :: Env -> [Exp] -> ([Exp], Env)
+renameExps env exps = case exps of
   (exp : es) ->
     let (exp', env')        = renameExp env exp
-        (nextExps, nextEnv) = renameExpsInBody env' es
+        (nextExps, nextEnv) = renameExps env' es
     in  (exp' : nextExps, nextEnv)
 
   [] ->
@@ -61,23 +77,23 @@ renameExpsInBody env exps = case exps of
 renameExp :: Env -> Exp -> (Exp, Env)
 renameExp env what = case what of
   Solved t area (TemplateString exps) ->
-    let renamedExps = fst . renameExp env <$> exps
-    in  (Solved t area (TemplateString renamedExps), env)
+    let (renamedExps, env') = renameExps env exps
+    in  (Solved t area (TemplateString renamedExps), env')
 
   Solved t area (App fn arg isFinal) ->
-    let (renamedFn, _)  = renameExp env fn
-        (renamedArg, _) = renameExp env arg
-    in  (Solved t area (App renamedFn renamedArg isFinal), env)
+    let (renamedFn, env')  = renameExp env fn
+        (renamedArg, env'') = renameExp env' arg
+    in  (Solved t area (App renamedFn renamedArg isFinal), env'')
 
   Solved t area (Access record field) ->
-    let (renamedRecord, _) = renameExp env record
-        (renamedField, _)  = renameExp env field
-    in  (Solved t area (Access renamedRecord renamedField), env)
+    let (renamedRecord, env') = renameExp env record
+        (renamedField, env'')  = renameExp env' field
+    in  (Solved t area (Access renamedRecord renamedField), env'')
 
   Solved t area (Abs (Solved paramType paramArea paramName) body) ->
     let env'             = extendScope paramName paramName env
-        (renamedBody, _) = renameExpsInBody env' body
-    in  (Solved t area (Abs (Solved paramType paramArea paramName) renamedBody), env)
+        (renamedBody, env'') = renameExps env' body
+    in  (Solved t area (Abs (Solved paramType paramArea paramName) renamedBody), env'')
 
   Solved t area (Assignment name exp) ->
     -- here we deal with an assignment in a body or Do
@@ -86,9 +102,25 @@ renameExp env what = case what of
         (renamedExp, _) = renameExp env exp
     in  (Solved t area (Assignment name renamedExp), env')
 
-  Solved t area (Var name) ->
-    let renamed = Maybe.fromMaybe name $ Map.lookup name (namesInScope env)
-    in  (Solved t area (Var renamed), env)
+  Solved t area (Var name) -> case break (== '.') name of
+    -- A normal name
+    (n, []) ->
+      let renamed = Maybe.fromMaybe name $ Map.lookup name (namesInScope env)
+      in  (Solved t area (Var renamed), env)
+
+    -- A field access name eg. Var ".field"
+    ([], fieldAccessor) ->
+      (Solved t area (Var fieldAccessor), env)
+
+    -- A namespace access eg. Var "List.filter"
+    (namespace, '.' : n) ->
+      let moduleHash = Maybe.fromMaybe namespace $ Map.lookup namespace (defaultImportHashes env)
+          renamed    = addHashToName moduleHash n
+          env'       = addDefaultImportNameUsage namespace n env
+      in  (Solved t area (Var renamed), env')
+
+    _ ->
+      undefined
 
   Solved t area (NameExport name) ->
     let renamed = Maybe.fromMaybe name $ Map.lookup name (namesInScope env)
@@ -99,46 +131,57 @@ renameExp env what = case what of
     in  (Solved t area (TypedExp renamedExp typing scheme), env)
 
   Solved t area (ListConstructor items) ->
-    let renamedItems = renameListItem env <$> items
-    in  (Solved t area (ListConstructor renamedItems), env)
+    let (renamedItems, env') = renameListItems env items
+    in  (Solved t area (ListConstructor renamedItems), env')
 
   Solved t area (TupleConstructor items) ->
     let renamedItems = fst . renameExp env <$> items
     in  (Solved t area (TupleConstructor renamedItems), env)
 
   Solved t area (Record fields) ->
-    let renamedFields = renameField env <$> fields
-    in  (Solved t area (Record renamedFields), env)
+    let (renamedFields, env') = renameFields env fields
+    in  (Solved t area (Record renamedFields), env')
 
   Solved t area (If cond truthy falsy) ->
-    let (renamedCond, _)   = renameExp env cond
-        (renamedTruthy, _) = renameExp env truthy
-        (renamedFalsy, _)  = renameExp env falsy
-    in  (Solved t area (If renamedCond renamedTruthy renamedFalsy), env)
+    let (renamedCond, env')     = renameExp env cond
+        (renamedTruthy, env'')  = renameExp env' truthy
+        (renamedFalsy, env''')  = renameExp env'' falsy
+    in  (Solved t area (If renamedCond renamedTruthy renamedFalsy), env''')
 
   Solved t area (Do exps) ->
-    let (renamedExps, _) = renameExpsInBody env exps
-    in  (Solved t area (Do renamedExps), env)
+    let (renamedExps, env') = renameExps env exps
+    in  (Solved t area (Do renamedExps), env')
 
   Solved t area (Where exp iss) ->
-    let (renamedExp, _) = renameExp env exp
-        renamedIss      = renameBranch env <$> iss
-    in  (Solved t area (Where renamedExp renamedIss), env)
+    let (renamedExp, env')  = renameExp env exp
+        (renamedIss, env'') = renameBranches env' iss
+    in  (Solved t area (Where renamedExp renamedIss), env'')
 
   Solved t area (Placeholder ph exp) ->
-    let (renamedExp, _) = renameExp env exp
-    in  (Solved t area (Placeholder ph renamedExp), env)
+    let (renamedExp, env') = renameExp env exp
+    in  (Solved t area (Placeholder ph renamedExp), env')
 
   _ ->
     (what, env)
 
 
-renameBranch :: Env -> Is -> Is
+renameBranches :: Env -> [Is] -> ([Is], Env)
+renameBranches env branches = case branches of
+  (branch : next) ->
+    let (renamed, env') = renameBranch env branch
+        (next', env'')  = renameBranches env' next
+    in  (renamed : next', env'')
+
+  [] ->
+    ([], env)
+
+
+renameBranch :: Env -> Is -> (Is, Env)
 renameBranch env is = case is of
   Solved t area (Is pat exp) ->
     let renamedPattern  = renamePattern env pat
-        (renamedExp, _) = renameExp env exp
-    in  Solved t area (Is renamedPattern renamedExp)
+        (renamedExp, env') = renameExp env exp
+    in  (Solved t area (Is renamedPattern renamedExp), env')
 
   _ ->
     undefined
@@ -171,29 +214,50 @@ renamePattern env pat = case pat of
     other
 
 
-renameField :: Env -> Field -> Field
+renameFields :: Env -> [Field] -> ([Field], Env)
+renameFields env fields = case fields of
+  (field : next) ->
+    let (renamed, env') = renameField env field
+        (next', env'')  = renameFields env' next
+    in  (renamed : next', env'')
+
+  [] ->
+    ([], env)
+
+
+renameField :: Env -> Field -> (Field, Env)
 renameField env field = case field of
   Solved t area (Field (name, exp)) ->
-    let (renamedExp, _) = renameExp env exp
-    in  Solved t area $ Field (name, renamedExp)
+    let (renamedExp, env') = renameExp env exp
+    in  (Solved t area $ Field (name, renamedExp), env')
 
   Solved t area (FieldSpread exp) ->
-    let (renamedExp, _) = renameExp env exp
-    in  Solved t area $ FieldSpread renamedExp
+    let (renamedExp, env') = renameExp env exp
+    in  (Solved t area $ FieldSpread renamedExp, env')
 
   _ ->
     undefined
 
 
-renameListItem :: Env -> ListItem -> ListItem
+renameListItems :: Env -> [ListItem] -> ([ListItem], Env)
+renameListItems env lis = case lis of
+  (li : next) ->
+    let (renamed, env') = renameListItem env li
+        (next', env'')  = renameListItems env' next
+    in  (renamed : next', env'')
+
+  [] ->
+    ([], env)
+
+renameListItem :: Env -> ListItem -> (ListItem, Env)
 renameListItem env item = case item of
   Solved t area (ListItem exp) ->
-    let (renamedExp, _) = renameExp env exp
-    in  Solved t area $ ListItem renamedExp
+    let (renamedExp, env') = renameExp env exp
+    in  (Solved t area $ ListItem renamedExp, env')
 
   Solved t area (ListSpread exp) ->
-    let (renamedExp, _) = renameExp env exp
-    in  Solved t area $ ListSpread renamedExp
+    let (renamedExp, env') = renameExp env exp
+    in  (Solved t area $ ListSpread renamedExp, env')
 
   _ ->
     undefined
@@ -327,11 +391,16 @@ renameImport env imp = case imp of
         (renamedNames, env') = renameSolvedNames env moduleHash names
     in  (Untyped area (NamedImport renamedNames relPath absPath), env')
 
-  -- TODO: this won't work like this as we need to rename and generate externs for what is actually used
-  Untyped area (DefaultImport name relPath absPath) ->
-    let moduleHash          = generateHashFromPath absPath
-        (renamedName, env') = renameSolvedName env moduleHash name
-    in  (Untyped area (DefaultImport renamedName relPath absPath), env')
+  {-
+    Here we just push the import hash to the env so that we can pull that information to rewrite say:
+    Var "List.filter" into Var "__hash_of_List__filter". Later this import will be rewritten as:
+    import { __hash_of_List__filter } from "List". Essentially we rewrite all default imports into
+    named imports.
+  -}
+  Untyped area (DefaultImport n@(Untyped _ name) relPath absPath) ->
+    let moduleHash = generateHashFromPath absPath
+        env'       = addDefaultImportHash name moduleHash env
+    in  (Untyped area (DefaultImport n relPath absPath), env')
 
   _ ->
     undefined
@@ -383,18 +452,38 @@ populateInitialEnv exps env = case exps of
     env
 
 
+rewriteDefaultImports :: Env -> [Import] -> [Import]
+rewriteDefaultImports env imports = case imports of
+  (Untyped area (DefaultImport (Untyped area' namespace) absPath relPath) : next) ->
+    let usedNames      = Maybe.fromMaybe Set.empty $ Map.lookup namespace (usedDefaultImportNames env)
+        hashFromModule = Maybe.fromMaybe "" $ Map.lookup namespace (defaultImportHashes env)
+        hashedNames    = Set.toList $ Set.map (Untyped area' . addHashToName hashFromModule) usedNames
+        next'          = rewriteDefaultImports env next
+    in  Untyped area (NamedImport hashedNames absPath relPath) : next'
+
+  (imp : next) ->
+    imp : rewriteDefaultImports env next
+
+  [] ->
+    []
+
 
 renameAST :: Env -> AST -> (AST, Env)
 renameAST env ast =
   let moduleHash                 = hashModulePath ast
       env'                       = populateInitialEnv (aexps ast) env { currentModuleHash = moduleHash }
-      (renamedImports, env'')    = renameImports (trace ("ENV': " <> ppShow env') env') $ aimports ast
+      (renamedImports, env'')    = renameImports env' $ aimports ast
       (renamedTypeDecls, env''') = renameTypeDecls env'' $ atypedecls ast
       (renamedExps, env'''')     = renameTopLevelExps env''' $ aexps ast
       renamedInstances           = renameInstance env'''' <$> ainstances ast
-  in  (ast { aexps = renamedExps, atypedecls = renamedTypeDecls, ainstances = renamedInstances, aimports = renamedImports }, env)
+      rewrittenImports           = rewriteDefaultImports env'''' renamedImports
+  in  (ast { aexps = (trace ("ENV'''': "<>ppShow env'''') renamedExps), atypedecls = renamedTypeDecls, ainstances = renamedInstances, aimports = rewrittenImports }, env)
 
 renameTable :: Table -> Table
 renameTable table =
-  let env       = Env { namesInScope = Map.empty, currentModuleHash = "" }
+  let env = Env { namesInScope = Map.empty
+                , currentModuleHash = ""
+                , usedDefaultImportNames = Map.empty
+                , defaultImportHashes = Map.empty
+                }
   in  Map.map (fst . renameAST env) table
