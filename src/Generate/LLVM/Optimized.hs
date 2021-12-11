57c3639c38553255ccd819f155974bee2ead41a0
Arnaud Boeglin <a.boeglin67@gmail.com>
2021-12-11 10:01:03 +0100

    fix: Fix methods having sub placeholders that were not stripped

--- src/Generate/LLVM/Optimized.hs
+++ src/Generate/LLVM/Optimized.hs
@@ -10,7 +10,15 @@ data Optimized a
   = Optimized (Ty.Qual Ty.Type) Area a
   -- = Optimized Ty.Type Area a
   | Untyped Area a
-  deriving(Eq, Show, Ord)
+  deriving(Eq, Ord)
+
+instance Show a => Show (Optimized a) where
+  show solved = case solved of
+    Optimized _ _ a ->
+      show a
+
+    Untyped _ a ->
+      show a
 
 data AST =
   AST
