3b0fd409dd2ab5dd62fe7b2a79998cbfaa49a8fd
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2022-01-12 20:01:37 +0100

    feat: Add request body to http runtime

--- runtime/src/http.hpp
+++ runtime/src/http.hpp
@@ -2,6 +2,7 @@
 #define HTTP_H
 
 #include "apply-pap.hpp"
+#include "record.hpp"
 
 
 /**
@@ -13,14 +14,42 @@ typedef struct madlib__http__Header {
   char **value;
 } madlib__http__Header_t;
 
+/**
+ * alias Body = Maybe String
+ */
+typedef struct madlib__http__Body {
+  /**
+   * 0: Just
+   * 1: Nothing
+   */
+  int64_t index;
+  void *bodyData;
+} madlib__http__Body_t;
+
+/**
+ * type Method
+ *   = CONNECT
+ *   | DELETE
+ *   | GET
+ *   | HEAD
+ *   | OPTIONS
+ *   | PATCH
+ *   | POST
+ *   | PUT
+ *   | TRACE
+ */
+typedef struct madlib__http__Method {
+  int64_t methodIndex;
+} madlib__http__Method_t;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /**
- * madlib__http__request :: String -> (Response -> ()) -> ()
+ * madlib__http__request :: Request -> (Response -> ()) -> ()
  */
-void madlib__http__request(char *url, PAP_t *callback);
+void madlib__http__request(madlib__record__Record_t *request, PAP_t *callback);
 
 #ifdef __cplusplus
 }
