83919fc2e3c8919844e56c30fed738be05f8001a
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2022-02-11 00:14:40 +0100

    feat: WIP trampolines

--- runtime/src/recursion.cpp
+++ runtime/src/recursion.cpp
@@ -7,9 +7,11 @@
 extern "C" {
 #endif
 
-static int NEXT = 0x00;
-static int DONE = 0x01;
-static int THUNK = 0x02;
+
+
+static int NEXT = 0x01;
+static int DONE = 0x02;
+static int THUNK = 0x03;
 
 typedef struct Trampoline {
   int tag;
@@ -21,28 +23,51 @@ void *id(void *x) {
 }
 
 void *madlib__recursion__internal__trampoline__1(PAP_t *fn, void *arg) {
-  printf("trampo_1\n");
+  int one = 1;
   PAP_t idPAP = {
     .fn = (void*)id,
     .arity = 1,
     .missingArgCount = 1,
     .env = NULL
   };
+
   Trampoline_t *v = (Trampoline_t*)__applyPAP__(fn, 2, arg, &idPAP);
-//   printf("trampo tag: %d\n", v->tag);
+
   while (v->tag == NEXT) {
     v = (Trampoline_t*)__applyPAP__(v->data, 1, NULL);
-    // printf("trampo tag: %d\n", v->tag);
   }
-  return NULL;
+
+  v = (Trampoline_t*)v->data;
+
+  while (v->tag == THUNK) {
+    // printf("tag: %d\n", v->tag);
+    // printf("data: %d\n", v->data);
+    v = (Trampoline_t*)__applyPAP__(v->data, 1, &one);
+  }
+
+  return (void*)v;
 }
 
 void *madlib__recursion__internal__Next(PAP_t *next) {
-//   printf("next\n");
-
   Trampoline_t *trampoline = (Trampoline_t*)GC_malloc(sizeof(Trampoline_t));
   trampoline->data = next;
   trampoline->tag = NEXT;
+
+  return trampoline;
+}
+
+void *madlib__recursion__internal__Thunk(PAP_t *next) {
+  Trampoline_t *trampoline = (Trampoline_t*)GC_malloc(sizeof(Trampoline_t));
+  trampoline->data = next;
+  trampoline->tag = THUNK;
+
+  return trampoline;
+}
+
+void *madlib__recursion__internal__Done(void *result) {
+  Trampoline_t *trampoline = (Trampoline_t*)GC_malloc(sizeof(Trampoline_t));
+  trampoline->data = result;
+  trampoline->tag = DONE;
 
   return trampoline;
 }
--- src/Optimize/Recursion.hs
+++ src/Optimize/Recursion.hs
@@ -3,6 +3,8 @@ module Optimize.Recursion where
 import AST.Core
 import Infer.Type
 import Explain.Location
+import Text.Show.Pretty
+import Debug.Trace
 
 
 convertTable :: Table -> Table
@@ -26,7 +28,7 @@ arityToFnType arity =
 
 
 
-generateTrampolinedWrapper :: Int -> Name -> Qual Type -> Exp
+generateTrampolinedWrapper :: Int -> String -> Qual Type -> Exp
 generateTrampolinedWrapper arity fnName qt@(_ :=> t) =
   let params         = arityToParamNames arity
       trampolineArgs = Typed ([] :=> arityToFnType arity) emptyArea (Var $ cpsName fnName) : (Typed ([] :=> tVar "a") emptyArea . Var <$> params)
@@ -109,29 +111,73 @@ nextQualType :: Qual Type
 nextQualType =
   [] :=> ((tVar "a" `fn` tVar "b") `fn` tVar "c")
 
-continuationType :: Qual Type
+continuationType :: Type
 continuationType =
-  [] :=> (tVar "a" `fn` tVar "b")
+  tVar "a" `fn` tVar "b"
+
+continuationQualType :: Qual Type
+continuationQualType =
+  [] :=> continuationType
 
 trampolineQualType :: Qual Type
 trampolineQualType =
   [] :=> TCon (TC "Trampoline" Star) "internal"
 
-wrapNext :: Exp -> Exp
-wrapNext exp =
-  Typed trampolineQualType emptyArea (Call SimpleCall (Typed nextQualType emptyArea (Var "madlib__recursion__internal__Next")) [
-    Typed callbackQualType emptyArea (Definition BasicDefinition ["__skip__"] [exp])
+
+data WrapperType
+  = Thunk
+  | Done
+  | Next
+
+wrap :: WrapperType -> Exp -> Exp
+wrap wrapperType exp = case wrapperType of
+  Next ->
+    Typed trampolineQualType emptyArea (Call SimpleCall (Typed nextQualType emptyArea (Var "madlib__recursion__internal__Next")) [
+      Typed callbackQualType emptyArea (Definition BasicDefinition ["__skip__"] [exp])
+    ])
+
+  Done ->
+    Typed ([] :=> tVar "a") emptyArea (Call SimpleCall (Typed nextQualType emptyArea (Var "madlib__recursion__internal__Done")) [Typed ([] :=> tVar "a") emptyArea (Call SimpleCall kExp [exp])])
+
+  _ ->
+    undefined
+
+kParamName :: String
+kParamName =
+  "__$kParam__"
+
+kName :: String
+kName =
+  "__$k__"
+
+kExp :: Exp
+kExp =
+  Typed continuationQualType emptyArea (Var kName)
+
+varExp :: String -> Exp
+varExp name =
+  Typed ([] :=> tVar "a") emptyArea (Var name)
+
+thunkifiedKExp :: Exp
+thunkifiedKExp =
+  Typed callbackQualType emptyArea (Definition BasicDefinition [kParamName] [
+    Typed trampolineQualType emptyArea (Call SimpleCall (Typed nextQualType emptyArea (Var "madlib__recursion__internal__Thunk")) [
+      Typed callbackQualType emptyArea (Definition BasicDefinition ["__skip__"] [varExp kParamName])
+    ])
+  ])
+
+thunkifyKExp :: Exp -> Exp
+thunkifyKExp exp =
+  Typed ([] :=> (getType exp `fn` tVar "b")) emptyArea (Definition BasicDefinition [kParamName] [
+    Typed trampolineQualType emptyArea (Call SimpleCall (Typed nextQualType emptyArea (Var "madlib__recursion__internal__Thunk")) [
+      Typed callbackQualType emptyArea (Definition BasicDefinition ["__skip__"] [Typed ([] :=> tVar "a") emptyArea (Call SimpleCall kExp [exp])])
+    ])
   ])
 
 
-kName :: String
-kName = "__$k__"
-
-kExp :: Exp
-kExp = Typed continuationType emptyArea (Var kName)
-
 cpsName :: String -> String
-cpsName name = "__" <> name <> "_CPS__"
+cpsName name =
+  "__" <> name <> "_CPS__"
 
 buildFnTypeWithContinuation :: Type -> Type
 buildFnTypeWithContinuation t =
@@ -145,12 +191,20 @@ convert fnName arity exp = case exp of
     if fnName /= fnName' then
       Typed qt area (Call callType (Typed qt' area' (Var fnName')) (convert fnName arity <$> args))
     else
-      wrapNext
-        (Typed qt area (Call callType (Typed qt' area' (Var $ cpsName fnName')) ((convert fnName arity <$> args) ++ [kExp])))
+      wrap
+        Next
+        (Typed qt area (Call callType (Typed qt' area' (Var $ cpsName fnName')) ((convert fnName arity <$> args) ++ [thunkifiedKExp])))
+
+  -- TODO: temporary, we need to properly look for leafs in if and where exps
+  Typed qt area (Call _ _ _) ->
+    exp
 
   Typed qt area (Assignment name (Typed qt' area' (Definition defType params args))) ->
     if name == fnName then
-      Typed qt area (Assignment (cpsName name) (Typed qt' area' (Definition defType (params ++ [kName]) (convert fnName arity <$> args))))
+      let returnType = getReturnType (getQualified qt')
+          newTypes   = getParamTypes (getQualified qt') ++ [continuationType, returnType]
+          newType    = preds qt' :=> foldr1 fn newTypes
+      in  Typed newType area (Assignment (cpsName name) (Typed newType area' (Definition defType (params ++ [kName]) (convert fnName arity <$> args))))
     else
       Typed qt area (Assignment name (Typed qt' area' (Definition defType params (convert fnName arity <$> args))))
 
@@ -160,14 +214,77 @@ convert fnName arity exp = case exp of
   Typed qt area (Definition defType params args) ->
     Typed qt area (Definition defType params (convert fnName arity <$> args))
 
+  Typed qt area (If cond truthy falsy) ->
+    -- let ()
+    -- in  undefined
+    Typed qt area (If
+      cond
+      (convert fnName arity truthy)
+      (convertLeaf fnName arity falsy)
+      -- (convert fnName arity falsy)
+    )
+
   _ ->
+    wrap Done exp
+
+
+convertLeaf :: String -> Int -> Exp -> Exp
+convertLeaf fnName arity exp = case searchForRecursion fnName arity exp of
+  (converted, Recursive args qt) ->
+    wrap
+      Next
+      (Typed (preds qt :=> getReturnType (getQualified qt)) emptyArea (Call SimpleCall (Typed qt emptyArea (Var $ cpsName fnName)) (args ++ [thunkifyKExp converted])))
+
+  (_, Final) ->
+    wrap Done exp
+
+  (_, NotFinal) ->
     exp
+
+
+data RecursionSearchResult
+  = Recursive [Exp] (Qual Type)
+  | Final
+  | NotFinal
+
+
+searchArgs :: String -> Int -> [Exp] -> ([Exp], RecursionSearchResult)
+searchArgs fnName arity exps = case exps of
+  (exp : more) ->
+    case searchForRecursion fnName arity exp of
+      (exp', Recursive args qt) ->
+        (exp' : more, Recursive args qt)
+
+      (exp', _) ->
+        let (more', result) = searchArgs fnName arity more
+        in  (exp : more', result)
+
+  [] ->
+    ([], NotFinal)
+
+searchForRecursion :: String -> Int -> Exp -> (Exp, RecursionSearchResult)
+searchForRecursion fnName arity exp = case exp of
+  Typed qt area (Call callType fn args) ->
+    case fn of
+      Typed qt' area' (Var name) | name == fnName ->
+        (Typed qt area' (Var kParamName), Recursive args qt')
+
+      _ ->
+        let nextFn             = fst $ searchForRecursion fnName arity fn
+            (nextArgs, result) = searchArgs fnName arity args
+        in  (Typed qt area (Call callType nextFn nextArgs), result)
+
+  _ ->
+    (exp, NotFinal)
 
 
 isRecursive :: Int -> String -> Exp -> Bool
 isRecursive arity functionName exp = case exp of
   Typed _ _ (Call _ (Typed _ _ (Var name)) args) | name == functionName && arity == length args ->
     True
+
+  Typed _ _ (Call _ fn args) ->
+    isRecursive arity functionName fn || any (isRecursive arity functionName) args
 
   Typed _ _ (Access rec _) ->
     isRecursive arity functionName rec
