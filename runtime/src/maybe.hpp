5d930db2b21fe13b333e1fd07533dfa3e66bf237
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2022-02-26 14:36:06 +0100

    fix: Fix minor regression introduced for FFI where llvm types would not match

--- runtime/src/maybe.hpp
+++ runtime/src/maybe.hpp
@@ -4,13 +4,9 @@
 const int64_t madlib__maybe__Maybe_JUST_INDEX = 0;
 const int64_t madlib__maybe__Maybe_NOTHING_INDEX = 1;
 
-typedef struct madlib__maybe__Maybe_Just {
+typedef struct madlib__maybe__Maybe {
   int64_t index;
   void *data;
-} madlib__maybe__Maybe_Just_t;
-
-typedef struct madlib__maybe__Maybe_Nothing {
-  int64_t index;
-} madlib__maybe__Maybe_Nothing_t;
+} madlib__maybe__Maybe_t;
 
 #endif // MAYBE_H
