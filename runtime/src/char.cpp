643951673f587c055fea6b4c2a98952fe57be2f1
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2022-03-20 20:24:03 +0100

    feat: Add mapChars for both targets

--- runtime/src/char.cpp
+++ runtime/src/char.cpp
@@ -1,9 +1,94 @@
-#include <gc.h>
 #include "char.hpp"
+
+#include <gc.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define isunicode(c) (((c)&0xc0) == 0xc0)
+
+char *utf8Encode(int32_t unicode) {
+  if (unicode <= 0x7f) {
+    // ASCII character
+    char *str = (char *)GC_malloc(sizeof(char) * 2);
+    str[0] = (char)unicode;
+    str[1] = '\0';
+    return str;
+  } else if (unicode <= 0x07ff) {
+    // 2-byte unicode
+    char *str = (char *)GC_malloc(sizeof(char) * 3);
+    str[0] = (char)(((unicode >> 6) & 0x1f) | 0xc0);
+    str[1] = (char)(((unicode >> 0) & 0x3f) | 0x80);
+    str[2] = '\0';
+    return str;
+  } else if (unicode <= 0xffff) {
+    // 3-byte unicode
+    char *str = (char *)GC_malloc(sizeof(char) * 4);
+    str[0] = (char)(((unicode >> 12) & 0x0f) | 0xe0);
+    str[1] = (char)(((unicode >> 6) & 0x3f) | 0x80);
+    str[2] = (char)(((unicode >> 0) & 0x3f) | 0x80);
+    str[3] = 0;
+    return str;
+  } else if (unicode <= 0x10ffff) {
+    // 4-byte unicode
+    char *str = (char *)GC_malloc(sizeof(char) * 5);
+    str[0] = (char)(((unicode >> 18) & 0x07) | 0xf0);
+    str[1] = (char)(((unicode >> 12) & 0x3f) | 0x80);
+    str[2] = (char)(((unicode >> 6) & 0x3f) | 0x80);
+    str[3] = (char)(((unicode >> 0) & 0x3f) | 0x80);
+    str[4] = '\0';
+    return str;
+  } else {
+    // or use replacement character
+    char *str = (char *)GC_malloc(sizeof(char) * 4);
+    str[0] = (char)0xef;
+    str[1] = (char)0xbf;
+    str[2] = (char)0xbf;
+    str[3] = '\0';
+    return str;
+  }
+}
+
+int32_t utf8DecodeChar(const char *str, int *i) {
+  const unsigned char *s = (const unsigned char *)str;
+  int32_t u = *s;
+  int l = 1;
+  if (isunicode(u)) {
+    int a = (u & 0x20)
+                ? ((u & 0x10) ? ((u & 0x08) ? ((u & 0x04) ? 6 : 5) : 4) : 3)
+                : 2;
+    if (a < 6 || !(u & 0x02)) {
+      int b, p = 0;
+      u = ((u << (a + 1)) & 0xff) >> (a + 1);
+      for (b = 1; b < a; ++b) u = (u << 6) | (s[l++] & 0x3f);
+    }
+  }
+  if (i) *i += l;
+  return u;
+}
+
+int32_t *utf8Decode(char *str) {
+  int l;
+  int i = 0;
+  int outputIndex = 0;
+  size_t strLength = strlen(str);
+  int32_t *output = (int32_t*)GC_malloc(sizeof(int32_t) * (strLength + 1));
+  while (str[i] != '\0') {
+    if (!isunicode(str[i])) {
+      output[outputIndex] = str[i];
+      i++;
+    } else {
+      l = 0;
+      output[outputIndex] = utf8DecodeChar(&str[i], &l);
+      i += l;
+    }
+    outputIndex++;
+  }
+  output[outputIndex] = 0;
+
+  return output;
+}
 
 bool *madlib__char__internal__eq(int32_t *a, int32_t *b) {
   bool *boxed = (bool *)GC_malloc(sizeof(bool));
@@ -11,52 +96,26 @@ bool *madlib__char__internal__eq(int32_t *a, int32_t *b) {
   return boxed;
 }
 
-char **madlib__char__internal__inspect(int32_t *unicode) {
-  char **boxed = (char **)GC_malloc(sizeof(char*));
+char **madlib__char__internal__show(int32_t *unicode) {
+  char **boxed = (char **)GC_malloc(sizeof(char *));
 
-  if (*unicode <= 0x7f) {
-    // ASCII character
-    char *str = (char *)GC_malloc(sizeof(char) * 2);
-    str[0] = (char)*unicode;
-    str[1] = '\0';
-    *boxed = str;
-  }
-  else if (*unicode <= 0x07ff) {
-    // 2-byte unicode
-    char *str = (char *)GC_malloc(sizeof(char) * 3);
-    str[0] = (char) (((*unicode >> 6) & 0x1f) | 0xc0);
-    str[1] = (char) (((*unicode >> 0) & 0x3f) | 0x80);
-    str[2] = '\0';
-    *boxed = str;
-  }
-  else if (*unicode <= 0xffff) {
-    // 3-byte unicode
-    char *str = (char *)GC_malloc(sizeof(char) * 4);
-    str[0] = (char) (((*unicode >> 12) & 0x0f) | 0xe0);
-    str[1] = (char) (((*unicode >>  6) & 0x3f) | 0x80);
-    str[2] = (char) (((*unicode >>  0) & 0x3f) | 0x80);
-    str[3] = 0;
-    *boxed = str;
-  }
-  else if (*unicode <= 0x10ffff) {
-    // 4-byte unicode
-    char *str = (char *)GC_malloc(sizeof(char) * 5);
-    str[0] = (char) (((*unicode >> 18) & 0x07) | 0xf0);
-    str[1] = (char) (((*unicode >> 12) & 0x3f) | 0x80);
-    str[2] = (char) (((*unicode >>  6) & 0x3f) | 0x80);
-    str[3] = (char) (((*unicode >>  0) & 0x3f) | 0x80);
-    str[4] = '\0';
-    *boxed = str;
-  }
-  else { 
-    // or use replacement character
-    char *str = (char *)GC_malloc(sizeof(char) * 4);
-    str[0] = (char) 0xef;  
-    str[1] = (char) 0xbf;
-    str[2] = (char) 0xbf;
-    str[3] = '\0';
-    *boxed = str;
-  }
+  *boxed = utf8Encode(*unicode);
+
+  return boxed;
+}
+
+char **madlib__char__internal__inspect(int32_t *unicode) {
+  char **boxed = (char **)GC_malloc(sizeof(char *));
+
+  char *encoded = utf8Encode(*unicode);
+  size_t encodedLength = strlen(encoded);
+  char *full = (char*)GC_malloc(sizeof(char) * (encodedLength + 3));
+  full[0] = '\'';
+  memcpy(full + 1, encoded, encodedLength);
+  full[1 + encodedLength] = '\'';
+  full[2 + encodedLength] = '\0';
+
+  *boxed = full;
 
   return boxed;
 }
--- runtime/src/string.hpp
+++ runtime/src/string.hpp
@@ -1,9 +1,12 @@
 #ifndef STRING_H
 #define STRING_H
 
+#include "apply-pap.hpp"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 
 bool *madlib__string__internal__eq(char **s1, char **s2);
 
@@ -17,6 +20,8 @@ int64_t madlib__string__length(char *s);
 char *madlib__string__internal__concat(char *s1, char *s2);
 
 char *stripTrailingZeros(char *number);
+
+char *madlib__string__mapChars(PAP_t *pap, char *str);
 
 #ifdef __cplusplus
 }
