5a77f0902c28b3918f9f2c0dd480b2882dd56193
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2022-03-20 20:24:03 +0100

    feat: Add mapChars for both targets

--- runtime/src/char.hpp
+++ runtime/src/char.hpp
@@ -10,9 +10,14 @@ extern "C" {
 
 bool *madlib__char__internal__eq(int32_t *a, int32_t *b);
 
-char **madlib__char__internal__inspect(int32_t *c);
+char **madlib__char__internal__show(int32_t *c);
+char **madlib__char__internal__inspect(int32_t *unicode);
 
 int64_t madlib__char__compare(int32_t c1, int32_t c2);
+
+char *utf8Encode(int32_t unicode);
+
+int32_t *utf8Decode(char *str);
 
 #ifdef __cplusplus
 }
