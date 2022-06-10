b2815e432652fa886f4b63bc49e1d31bc8c9b022
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2022-06-10 21:41:05 +0200

    feat: Good progress on language server

--- compiler/Run/Format.hs
+++ compiler/Run/Format.hs
@@ -17,84 +17,88 @@ import           Error.Error
 import           Format.Format
 
 import Text.Show.Pretty
+import qualified Rock
+import Driver.Query
+import Run.Options
+import Parse.Madlib.Grammar (parse)
+import Data.Either.Combinators (mapLeft)
 
 
 
 
--- parseASTsToFormat :: [FilePath] -> IO (Either CompilationError [(AST, [Comment])])
--- parseASTsToFormat        []         = return $ Right []
--- parseASTsToFormat  (fp : fps)   = do
---   canonicalEntrypoint <- Directory.canonicalizePath fp
---   code <- try $ readFile defaultPathUtils canonicalEntrypoint :: IO (Either IOException String)
+parseASTsToFormat :: [FilePath] -> IO (Either CompilationError [(AST, [Comment])])
+parseASTsToFormat        []         = return $ Right []
+parseASTsToFormat  (fp : fps)   = do
+  canonicalEntrypoint <- Directory.canonicalizePath fp
+  code <- try $ readFile defaultPathUtils canonicalEntrypoint :: IO (Either IOException String)
 
---   let source = case code of
---           Right a -> Right a
---           Left  _ -> Left $ CompilationError (ImportNotFound canonicalEntrypoint) NoContext
+  let source = case code of
+          Right a -> Right a
+          Left  _ -> Left $ CompilationError (ImportNotFound canonicalEntrypoint) NoContext
 
---   case source of
---       Left e ->
---         return $ Left $ CompilationError (ImportNotFound canonicalEntrypoint) NoContext
+  case source of
+      Left e ->
+        return $ Left $ CompilationError (ImportNotFound canonicalEntrypoint) NoContext
 
---       Right code -> do
---         next <- parseASTsToFormat fps
---         ast <- buildAST defaultPathUtils canonicalEntrypoint code
---         let comments =
---               case parseComments code of
---                 Right a ->
---                   Right a
+      Right code -> do
+        next <- parseASTsToFormat fps
+        let ast = mapLeft (const $ CompilationError (GrammarError "" "") NoContext) (parse code)
+        let comments =
+              case parseComments code of
+                Right a ->
+                  Right a
 
---                 Left _ ->
---                   Left $ CompilationError Error NoContext
+                Left _ ->
+                  Left $ CompilationError Error NoContext
 
---         let astWithComments = (,) <$> ast <*> comments
+        let astWithComments = (,) <$> ((\a -> a {apath = Just ""}) <$> ast) <*> comments
 
---         return $ (:) <$> astWithComments <*> next
-
-
--- parseCodeToFormat :: String -> IO (Either CompilationError [(AST, [Comment])])
--- parseCodeToFormat code = do
---   ast <- buildAST defaultPathUtils "" code
---   let comments =
---         case parseComments code of
---           Right a ->
---             Right a
-
---           Left _ ->
---             Left $ CompilationError Error NoContext
---   return $ (\a cs -> [(a, cs)]) <$> ast <*> comments
+        return $ (:) <$> astWithComments <*> next
 
 
--- processAST :: Int -> Bool -> AST -> [Comment] -> IO String
--- processAST width fix ast comments = do
---   let formatted = astToSource width ast comments
+parseCodeToFormat :: String -> IO (Either CompilationError [(AST, [Comment])])
+parseCodeToFormat code = do
+  let ast = mapLeft (const $ CompilationError (GrammarError "" "") NoContext) (parse code)
+  let comments =
+        case parseComments code of
+          Right a ->
+            Right a
 
---   case apath ast of
---     Just path -> do
---       if fix then
---         writeFile path formatted
---       else
---         putStr formatted
+          Left _ ->
+            Left $ CompilationError Error NoContext
+  return $ (\a cs -> [(a, cs)]) <$> ((\a -> a {apath = Just ""}) <$> ast) <*> comments
 
---     Nothing ->
---       return ()
 
---   return formatted
+processAST :: Int -> Bool -> AST -> [Comment] -> IO String
+processAST width fix ast comments = do
+  let formatted = astToSource width ast comments
+
+  case apath ast of
+    Just path -> do
+      if fix then
+        writeFile path formatted
+      else
+        putStr formatted
+
+    Nothing ->
+      return ()
+
+  return formatted
 
 runFormatter :: Int -> Bool -> FilePath -> String -> IO ()
 runFormatter width fix path code = do
-  undefined
-  -- astsWithComments <-
-  --   if code == "--EMPTY--" then do
-  --     filesToFormat <- getFilesToCompile False path
-  --     parseASTsToFormat filesToFormat
-  --   else
-  --     parseCodeToFormat code
+  astsWithComments <-
+    if code == "--EMPTY--" then do
+      filesToFormat <- getFilesToCompile False path
+      parseASTsToFormat filesToFormat
+    else
+      parseCodeToFormat code
 
-  -- case astsWithComments of
-  --   Right asts' -> do
-  --     mapM_ (uncurry (processAST width fix)) asts'
-  --   Left err -> do
-  --     putStrLn $ ppShow err
-  --     exitFailure
+  case astsWithComments of
+    Right asts' -> do
+      mapM_ (uncurry (processAST width fix)) asts'
+    Left err -> do
+      putStrLn $ ppShow err
+      exitFailure
 
-  -- return ()
+  return ()
--- compiler/Run/LanguageServer.hs
+++ compiler/Run/LanguageServer.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Use lambda-case" #-}
-{-# LANGUAGE NamedFieldPuns #-}
+
 module Run.LanguageServer where
 
 -- import qualified Language.Haskell.LSP.Control           as LSP
@@ -41,32 +41,20 @@ import Explain.Location
 import qualified Explain.Location as Loc
 import Language.LSP.Diagnostics
 import qualified Explain.Format as Explain
-import Data.List (group)
+import Data.List (group, foldl')
 import qualified Data.List as List
 import Control.Monad (forM_)
+import Data.IORef
+import qualified AST.Solved as Slv
+import Explain.Format (prettyPrintQualType, prettyPrintType)
+import Control.Applicative ((<|>))
+import Infer.Type (Qual((:=>)), Type)
 
 
 handlers :: State -> Handlers (LspM ())
 handlers state = mconcat
-  [ notificationHandler SInitialized $ \_not -> do
-      -- let params = ShowMessageRequestParams MtInfo (T.pack "Turn on code lenses?")
-      --       (Just [MessageActionItem "Turn on", MessageActionItem "Don't"])
-      -- _ <- sendRequest SWindowShowMessageRequest params $ \res ->
-      --   case res of
-      --     Right (Just (MessageActionItem "Turn on")) -> do
-      --       let regOpts = CodeLensRegistrationOptions Nothing Nothing (Just False)
-
-      --       _ <- registerCapability STextDocumentCodeLens regOpts $ \_req responder -> do
-      --         let cmd = Command "Say hello" "lsp-hello-command" Nothing
-      --             rsp = List [CodeLens (mkRange 0 0 0 100) (Just cmd) Nothing]
-      --         responder (Right rsp)
-      --       pure ()
-      --     Right _ ->
-      --       sendNotification SWindowShowMessage (ShowMessageParams MtInfo "Not turning on code lenses")
-      --     Left err ->
-      --       sendNotification SWindowShowMessage (ShowMessageParams MtError $ "Something went wrong!\n" <> T.pack (show err))
-      sendNotification SWindowShowMessage (ShowMessageParams MtInfo "Initialized")
-      pure ()
+  [ notificationHandler SInitialized $ \_not ->
+      sendNotification SWindowLogMessage (LogMessageParams MtInfo "Madlib server initialized")
   , requestHandler STextDocumentHover $ \req responder -> do
       p <- getRootPath
       let path = case p of
@@ -75,21 +63,240 @@ handlers state = mconcat
 
             _ ->
               "no path"
-      let RequestMessage _ _ _ (HoverParams _doc pos _workDone) = req
-          Position _l _c' = pos
-          rsp = Hover ms (Just range)
-          ms = HoverContents $ markedUpContent "madlib" "hover"
-          range = Range pos pos
-      responder (Right $ Just rsp)
-  , notificationHandler STextDocumentDidOpen $ \(NotificationMessage _ _ (DidOpenTextDocumentParams (TextDocumentItem uri _ _ _))) -> do
-      generateDiagnostics state uri
+      let RequestMessage _ _ _ (HoverParams (TextDocumentIdentifier uri) pos _workDone) = req
+          Position _l _c = pos
+      sendNotification SWindowLogMessage (LogMessageParams MtInfo (T.pack $ ppShow uri))
+      maybeHoverInfo <- liftIO $ getHoverInformation state (Loc 0 (_l + 1) (_c + 1)) (uriToPath uri)
+      case maybeHoverInfo of
+        Just info -> do
+          let ms    = HoverContents $ markedUpContent "Madlib" (T.pack info)
+              range = Range pos pos
+              rsp   = Hover ms (Just range)
+          responder (Right $ Just rsp)
 
-  , notificationHandler STextDocumentDidSave $ \(NotificationMessage _ _ (DidSaveTextDocumentParams (TextDocumentIdentifier uri) _)) -> do
-      generateDiagnostics state uri
+        Nothing ->
+          return ()
+
+  , notificationHandler STextDocumentDidOpen $ \(NotificationMessage _ _ (DidOpenTextDocumentParams (TextDocumentItem uri _ _ _))) ->
+      generateDiagnostics state uri mempty
+
+  , notificationHandler STextDocumentDidSave $ \(NotificationMessage _ _ (DidSaveTextDocumentParams (TextDocumentIdentifier uri) _)) ->
+      generateDiagnostics state uri mempty
+  , notificationHandler STextDocumentDidChange $ \(NotificationMessage _ _ (DidChangeTextDocumentParams (VersionedTextDocumentIdentifier uri _) (List changes))) -> do
+      let (TextDocumentContentChangeEvent _ _ docContent) = last changes
+      generateDiagnostics state uri (Map.singleton (uriToPath uri) (T.unpack docContent))
+      -- sendNotification SWindowLogMessage $ LogMessageParams MtInfo ("error count: " <> T.pack (ppShow uri))
+      -- sendNotification SWindowLogMessage $ LogMessageParams MtInfo ("error count: " <> T.pack (ppShow $ last changes))
   ]
 
 
--- publishDiagnostics :: MonadLsp config m => Int -> NormalizedUri -> TextDocumentVersion -> DiagnosticsBySource -> m ()
+
+isInRange :: Loc -> Area -> Bool
+isInRange (Loc _ l c) (Area (Loc _ lstart cstart) (Loc _ lend cend)) =
+  (l >= lstart && l <= lend)
+  && (not (l == lstart && c < cstart) && not (l == lend && c > cend))
+
+
+
+prettyQt :: Bool -> Qual Type -> String
+prettyQt topLevel qt@(_ :=> t) =
+  if topLevel then
+    prettyPrintQualType True qt
+  else
+    prettyPrintType True t
+
+
+data Node
+  = ExpNode Bool Slv.Exp
+  | NameNode Bool (Slv.Solved String)
+  | PatternNode (Slv.Pattern)
+
+
+findNodeAtLocInListItem :: Loc -> Slv.ListItem -> Maybe Node
+findNodeAtLocInListItem loc li = case li of
+  Slv.Typed _ _ (Slv.ListItem exp) ->
+    findNodeAtLoc False loc exp
+
+  Slv.Typed _ _ (Slv.ListSpread exp) ->
+    findNodeAtLoc False loc exp
+
+  _ ->
+    Nothing
+
+
+findNodeAtLocInField :: Loc -> Slv.Field -> Maybe Node
+findNodeAtLocInField loc field = case field of
+  Slv.Typed _ _ (Slv.Field (_, exp)) ->
+    findNodeAtLoc False loc exp
+
+  Slv.Typed _ _ (Slv.FieldSpread exp) ->
+    findNodeAtLoc False loc exp
+
+  _ ->
+    Nothing
+
+
+findNodeAtLocInPattern :: Loc -> Slv.Pattern -> Maybe Node
+findNodeAtLocInPattern loc input@(Slv.Untyped _ _) = Nothing
+findNodeAtLocInPattern loc input@(Slv.Typed qt area pat) =
+  if isInRange loc area then
+    let deeper =
+          case pat of
+            Slv.PCon name pats ->
+              foldl' (<|>) Nothing $ findNodeAtLocInPattern loc <$> pats
+
+            Slv.PList pats ->
+              foldl' (<|>) Nothing $ findNodeAtLocInPattern loc <$> pats
+
+            Slv.PTuple pats ->
+              foldl' (<|>) Nothing $ findNodeAtLocInPattern loc <$> pats
+
+            Slv.PRecord pats ->
+              foldl' (<|>) Nothing $ findNodeAtLocInPattern loc <$> pats
+
+            Slv.PSpread pat ->
+              findNodeAtLocInPattern loc pat
+
+            _ ->
+              Nothing
+    in  case deeper of
+          Nothing ->
+            Just $ PatternNode input
+
+          Just _ ->
+            deeper
+  else
+    Nothing
+
+
+findNodeAtLocInIs :: Loc -> Slv.Is -> Maybe Node
+findNodeAtLocInIs loc (Slv.Untyped _ _) = Nothing
+findNodeAtLocInIs loc (Slv.Typed _ _ (Slv.Is pat exp)) =
+  findNodeAtLocInPattern loc pat <|> findNodeAtLoc False loc exp
+
+
+findNodeAtLoc :: Bool -> Loc -> Slv.Exp -> Maybe Node
+findNodeAtLoc topLevel loc input@(Slv.Typed qt area exp) =
+  if isInRange loc area then
+    let deeper =
+          case exp of
+            Slv.App fn arg _ ->
+              findNodeAtLoc False loc arg <|> findNodeAtLoc False loc fn
+
+            Slv.Abs param@(Slv.Typed _ paramArea _) body ->
+              if isInRange loc paramArea then
+                Just $ NameNode False param
+              else
+                foldl' (<|>) Nothing $ findNodeAtLoc False loc <$> body
+
+            Slv.Do body ->
+              foldl' (<|>) Nothing $ findNodeAtLoc False loc <$> body
+
+            Slv.Assignment name exp ->
+              findNodeAtLoc False loc exp
+
+            Slv.Placeholder _ exp' ->
+              findNodeAtLoc False loc exp'
+
+            Slv.If cond truthy falsy ->
+              findNodeAtLoc False loc cond
+              <|> findNodeAtLoc False loc truthy
+              <|> findNodeAtLoc False loc falsy
+
+            Slv.Export exp' ->
+              findNodeAtLoc False loc exp'
+
+            Slv.TypedExp exp' _ _ ->
+              findNodeAtLoc False loc exp'
+
+            Slv.Var name _ ->
+              Just $ NameNode topLevel (Slv.Typed qt area name)
+
+            Slv.TemplateString exps ->
+              foldl' (<|>) Nothing $ findNodeAtLoc False loc <$> exps
+
+            Slv.TupleConstructor items ->
+              foldl' (<|>) Nothing $ findNodeAtLoc False loc <$> items
+
+            Slv.ListConstructor items ->
+              foldl' (<|>) Nothing $ findNodeAtLocInListItem loc <$> items
+
+            Slv.Record fields ->
+              foldl' (<|>) Nothing $ findNodeAtLocInField loc <$> fields
+
+            Slv.Where exp iss ->
+              findNodeAtLoc False loc exp
+              <|> foldl' (<|>) Nothing (findNodeAtLocInIs loc <$> iss)
+
+            _ ->
+              Nothing
+    in  case deeper of
+          Nothing | Slv.isPlaceholderExp input ->
+            Nothing
+
+          Nothing ->
+            Just $ ExpNode topLevel input
+
+          Just _ ->
+            deeper
+  else
+    Nothing
+findNodeAtLoc topLevel loc (Slv.Untyped _ _) =
+  Nothing
+
+
+
+
+findNodeForLocInExps :: Loc -> [Slv.Exp] -> Maybe Node
+findNodeForLocInExps loc exps = case exps of
+  exp : next ->
+    findNodeAtLoc True loc exp <|> findNodeForLocInExps loc next
+
+  [] ->
+    Nothing
+
+
+nodeToHoverInfo :: Node -> String
+nodeToHoverInfo node = case node of
+  ExpNode topLevel (Slv.Typed qt _ (Slv.Assignment name _)) ->
+    name <> " :: " <> prettyQt topLevel qt
+
+  ExpNode topLevel (Slv.Typed qt _ (Slv.TypedExp (Slv.Typed _ _ (Slv.Assignment name _)) _ _)) ->
+    name <> " :: " <> prettyQt topLevel qt
+
+  ExpNode topLevel (Slv.Typed qt _ (Slv.TypedExp (Slv.Typed _ _ (Slv.Export (Slv.Typed _ _ (Slv.Assignment name _)))) _ _)) ->
+    name <> " :: " <> prettyQt topLevel qt
+
+  NameNode topLevel (Slv.Typed qt _ name) ->
+    name <> " :: " <> prettyQt topLevel qt
+
+  ExpNode topLevel (Slv.Typed qt _ _) ->
+    prettyQt topLevel qt
+
+  PatternNode (Slv.Typed qt _ (Slv.PVar name)) ->
+    name <> " :: " <> prettyQt False qt
+
+  PatternNode (Slv.Typed qt _ _) ->
+    prettyQt False qt
+
+  _ ->
+    ""
+
+
+
+hoverInfoTask :: Loc -> FilePath -> Rock.Task Query.Query (Maybe String)
+hoverInfoTask loc path = do
+  (typedAst, _) <- Rock.fetch $ Query.SolvedASTWithEnv path
+  return $ nodeToHoverInfo <$> findNodeForLocInExps loc (Slv.aexps typedAst)
+
+
+
+
+getHoverInformation :: State -> Loc -> FilePath -> IO (Maybe String)
+getHoverInformation state loc path = do
+  (result, _) <- runTask state Driver.Don'tPrune mempty mempty (hoverInfoTask loc path)
+  return result
+
 
 uriToPath :: Uri -> FilePath
 uriToPath uri =
@@ -99,21 +306,20 @@ uriToPath uri =
       else
         unpacked
 
-generateDiagnostics :: State -> Uri -> LspM () ()
-generateDiagnostics state uri = do
+generateDiagnostics :: State -> Uri -> Map.Map FilePath String -> LspM () ()
+generateDiagnostics state uri fileUpdates = do
   let path = uriToPath uri
   -- retrieve current errors first, so that we can update diagnostics of files
   -- that went from errors > 0 to 0 and remove them completely.
-  (_, errs) <- liftIO $ runTask state Driver.Don'tPrune [path] (typeCheckFile path)
+  (_, errs) <- liftIO $ runTask state Driver.Don'tPrune [path] fileUpdates (typeCheckFile path)
   let errsByModule = groupErrsByModule errs
 
+  flushDiagnosticsBySource 20 (Just "Madlib")
 
   forM_ errsByModule $ \errs' -> do
     let moduleUri = uriOfError $ head errs'
     diagnostics <- liftIO $ mapM errorToDiagnostic errs'
     let diagnosticsBySource = partitionBySource diagnostics
-    -- sendNotification SWindowLogMessage $ LogMessageParams MtInfo ("errs: " <> T.pack (ppShow errs))
-    -- flushDiagnosticsBySource 20 (Just "Madlib")
     publishDiagnostics 20 (toNormalizedUri moduleUri) Nothing diagnosticsBySource
 
 
@@ -130,6 +336,20 @@ areErrorsFromSameModule a b = case (a, b) of
 
   _ ->
     False
+
+
+isErrorFromModule :: FilePath -> CompilationError -> Bool
+isErrorFromModule path err = case err of
+  CompilationError _ (Context ctxPath _ _) ->
+    ctxPath == path
+
+  _ ->
+    False
+
+
+errorsForModule :: [CompilationError] -> FilePath -> [CompilationError]
+errorsForModule errs path =
+  filter (isErrorFromModule path) errs
 
 
 groupErrsByModule :: [CompilationError] -> [[CompilationError]]
@@ -176,7 +396,7 @@ areaToRange area =
 
 data State = State
   { _driverState :: Driver.State CompilationError
-  , _openFiles :: Map.Map FilePath String
+  , _openFiles :: IORef (Map.Map FilePath String)
   , _changedFiles :: Set.Set FilePath
   }
 
@@ -194,7 +414,8 @@ textDocumentSyncOptions =
 runLanguageServer :: IO Int
 runLanguageServer = do
   driverState <- Driver.initialState
-  let state = State driverState mempty mempty
+  openFiles <- newIORef mempty
+  let state = State driverState openFiles mempty
   runServer $ ServerDefinition
     { defaultConfig = ()
     , onConfigurationChange = const $ pure $ Right ()
@@ -211,18 +432,11 @@ typeCheckFile path = do
   return ()
 
 
-runTask :: State -> Driver.Prune -> [FilePath] -> Rock.Task Query.Query a -> IO (a, [CompilationError])
-runTask state prune invalidatedPaths task = do
-  -- filePaths <- Rock.fetch $ Query.ModulePathsToBuild path
-  -- files <- mapM
-  --       (\path -> do
-  --           file <- Rock.fetch $ Query.File path
-  --           return (path, file)
-  --       )
-  --       filePaths
+runTask :: State -> Driver.Prune -> [FilePath] -> Map.Map FilePath String -> Rock.Task Query.Query a -> IO (a, [CompilationError])
+runTask state prune invalidatedPaths fileUpdates task =
   Driver.runIncrementalTask
-    (_driverState state)
-    invalidatedPaths
-    -- (_changedFiles state)
-    prune
-    task
+  (_driverState state)
+  invalidatedPaths
+  fileUpdates
+  prune
+  task
