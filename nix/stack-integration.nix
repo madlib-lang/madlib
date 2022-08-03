b745357f16d21e4a24e1282b7b972e79e023fbf7
Hlöðver Sigurðsson <hlolli@gmail.com>
2022-08-04 00:48:08 +0200

    feature(nix): fix wrong llvm-9.x library paths with llvm-config hack

--- nix/stack-integration.nix
+++ nix/stack-integration.nix
@@ -7,12 +7,48 @@ in
 # See https://docs.haskellstack.org/en/stable/nix_integration/#using-a-custom-shellnix-file
 { ghc }:
 
-pkgs.haskell.lib.buildStackProject {
+let llvm = pkgs.llvm_9.dev;
+
+    llvm-config-hack = pkgs.writeShellScriptBin "llvm-config" ''
+      if [[ "$1" =~ "version" ]]
+      then
+        echo 9.0.1
+      elif [[ "$1" =~ "cxx" ]]
+      then
+        echo "-I${llvm}/include -std=c++14 -stdlib=libc++ -fno-exceptions"
+      elif [[ "$1" =~ "includedir" ]]
+      then
+        echo "-I${llvm}/include"
+      elif [[ "$1" =~ "libdir" ]]
+      then
+        echo "${llvm}/lib"
+      elif [[ "$1" =~ "libs" ]]
+      then
+        echo "-lLLVM-9"
+       elif [[ "$1" =~ "cflags" ]]
+      then
+        echo "-I${llvm}/include"
+      else
+        exit $1
+      fi
+      exit 0
+    '';
+
+
+in pkgs.haskell.lib.buildStackProject {
   inherit ghc;
   name = "haskell-stack-nix";
+
+  PATH = "${llvm-config-hack}/bin:$PATH";
+  NIX_CFLAGS_COMPILE = "-Wno-deprecated -Wno-implicit-function-declaration -Wno-int-conversion";
+  HAVE_GETRANDOM = "1";
+
   # System dependencies needed at compilation time
   buildInputs = [
     pkgs.zlib
+    llvm-config-hack
+    llvm
+    pkgs.haskellPackages.alex
   ] ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
     Cocoa
     CoreServices
