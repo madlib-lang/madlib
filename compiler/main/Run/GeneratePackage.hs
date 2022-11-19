9cd4cbc947ad24c4851e1ea0e1ccd7859f57bae7
Arnaud BOEGLIN <a.boeglin67@gmail.com>
2022-11-19 20:36:20 +0100

    feat: Update package generator

--- compiler/main/Run/GeneratePackage.hs
+++ compiler/main/Run/GeneratePackage.hs
@@ -58,7 +58,15 @@ makeMadlibDotJson name version = unlines
   ["{", "  \"name\": \"" <> name <> "\",", "  \"version\": \"" <> version <> "\",", "  \"main\": \"src/Main.mad\"", "}"]
 
 mainDotMadContent :: String
-mainDotMadContent = unlines ["import IO from \"IO\"", "", "IO.log(\"Hello World!\")"]
+mainDotMadContent = unlines
+                      [ "import IO from \"IO\""
+                      , ""
+                      , ""
+                      , ""
+                      , "main = () => {"
+                      , "  IO.putLine(\"Hello World!\")"
+                      , "}"
+                      ]
 
 readme :: String -> String
 readme projectName = unlines
@@ -68,12 +76,22 @@ readme projectName = unlines
   , "### Notes on Madlib"
   , "Madlib is a general purpose programming language that compiles to Javascript. It means that you need to have [Nodejs](https://nodejs.org/) installed and configured in order to make it work. Madlib can target nodejs or browser, by default it will compile for nodejs."
   , "### How to run it"
-  , "First, you need to compile the program:"
+  , "First, you need to compile the program."
+  , "For node:"
   , "```shell"
   , "madlib compile -i src/Main.mad"
   , "```"
   , "Then, you can run it like this:"
   , "```shell"
   , "node build/Main.mjs"
+  , "```"
+  , ""
+  , "As a native binary:"
+  , "```shell"
+  , "madlib compile --target llvm -i src/Main.mad -o ./build/helloWorld"
+  , "```"
+  , "Then, you can run it like this:"
+  , "```shell"
+  , "./build/helloWorld"
   , "```"
   ]
